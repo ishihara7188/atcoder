71ddcac5dd5c71e7346abd9adf4a6f29a1bfe679
yokina <mail@cloxs.jp>
2019-01-04 13:36:13 +0900

    Create lunlun754.rb

---
+++ lunlun754.rb
@@ -0,0 +1,19 @@
+#https://atcoder.jp/contests/abc114/tasks/abc114_b
+#B - 754
+S = gets.strip
+
+target_number = [7,5,4];
+#answer
+nearest_n = [10,10,10]
+checking_position = 0
+
+p S
+target_number.size.times{
+    S.size.times{|i|
+    if nearest_n[checking_position] >= (target_number[checking_position] - S[i].to_i).abs
+        nearest_n[checking_position] = S[i].to_i
+    end
+    }
+}
+
+
