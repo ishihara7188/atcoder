1bcb88394af487d58f20b2736eb6647b38a90b4b
kaeruko <mail@cloxs.jp>
2019-01-05 13:10:51 +0900

    add palace.rb

--- eveeve.rb
+++ eveeve.rb
@@ -1,19 +1,24 @@
 #B - Christmas Eve Eve
 #https://atcoder.jp/contests/abc115/tasks/abc115_b
 
+
+n = gets.to _i
+p = n.times.map {|i| gets.to _i }
+puts p.inject(:+) - p.max / 2
+
 N = gets.strip.to_i
 p =  (0...N).map{gets.strip.to_i}
 
-pmax = p.max.floor
+# pmax = p.max.floor
 
-sum_p = 0
-used_coupon = false
-p.size.times{|i|
-	if pmax == p[i] && !used_coupon
-		sum_p += (p[i] * 0.5).floor
-		used_coupon = true
-	else
-		sum_p += p[i]
-	end
-}
-puts sum_p
+# sum_p = 0
+# used_coupon = false
+# p.size.times{|i|
+# 	if pmax == p[i] && !used_coupon
+# 		sum_p += (p[i] * 0.5).floor
+# 		used_coupon = true
+# 	else
+# 		sum_p += p[i]
+# 	end
+# }
+# puts sum_p
---
+++ palace.rb
@@ -0,0 +1,17 @@
+#https://atcoder.jp/contests/abc113/tasks/abc113_b
+#B - Palace
+gets
+T, A = gets.split(" ").map{|d|d.to_i}
+H = gets.split(" ").map{|d|d.to_i}
+
+temps = []
+min_t = (0..H.size-1).map{|n|
+	temps[n] = (A - (T - H[n] * 0.006)).abs
+}.min
+ret = 0
+(0..temps.size-1).map{|n|
+  if min_t == temps[n]
+    ret = n+1
+  end
+}
+puts ret
