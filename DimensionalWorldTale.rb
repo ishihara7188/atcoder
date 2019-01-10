3021a8232ca85b6526d0c998b4d976d58e3acbd2
kaeruko <mail@cloxs.jp>
2019-01-10 19:07:28 +0900

    modefied DimensionalWorldTale.rb

--- DimensionalWorldTale.rb
+++ DimensionalWorldTale.rb
@@ -4,11 +4,10 @@ n, m, x, y = gets.split(" ").map{|d|d.to_i}
 xlist = gets.split(" ").map{|d|d.to_i}.push(x)
 ylist = gets.split(" ").map{|d|d.to_i}.push(y)
 
-if (x...y).map{|i|
-    if xlist.select{|xi| xi < i }.size == xlist.size && ylist.select{|yi| yi >= i }.size == ylist.size
-    end
-}.size > 0
-    puts "No War"
-else
-    puts "War"
-end
+(x...y).map{|i|
+   if xlist.select{|xi| xi < i }.size == xlist.size && ylist.select{|yi| yi >= i }.size == ylist.size
+      puts "No War"
+      exit
+   end
+}
+puts "War"
