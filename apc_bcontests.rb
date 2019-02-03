96bea98c84726a7c2b268fc1b6a0c71f78b4ba90
totti <r.funk.7188@gmail.com>
2019-02-03 20:42:57 +0900

    add a

--- apc_bcontests.rb
+++ apc_bcontests.rb
@@ -2,3 +2,6 @@ n = gets.to_i
 a,b = gets.split(" ").map{|d|d.to_i}
 p = gets.split(" ").map{|d|d.to_i}
 p ([p.select{|i| i <= a }.size ,p.select{|i| a < i && b >= i }.size ,p.select{|i| b < i }.size].min)
+
+
+# aaaa
